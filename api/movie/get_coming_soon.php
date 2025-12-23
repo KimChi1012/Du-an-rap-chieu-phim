5ee1e87c6997520577ae1eee68fc9bcbf2b10458
lehien16 <lehien161205@gmail.com>
2025-12-23 09:23:29 +0700

    Thêm Phim sắp chiếu (Section)

---
+++ api/movie/get_coming_soon.php
@@ -0,0 +1,18 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+include "../db.php";
+
+$sql = "SELECT * FROM Phim WHERE TrangThai = 'Phim sắp chiếu'";
+$result = mysqli_query($conn, $sql);
+
+$movies = [];
+if ($result) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $movies[] = $row;
+    }
+}
+
+echo json_encode($movies, JSON_UNESCAPED_UNICODE);
+
+mysqli_close($conn);
+?>
